fc9e6a8a38660fc99e6e74d6f72e6e30720875ad
Andreas Reischl <andistar88@gmx.at>
2019-05-29 15:54:40 +0200

    main method edited

--- Reischl_Lauss_android/android.java
+++ Reischl_Lauss_android/android.java
@@ -1,5 +1,8 @@
 public class MainClass {
     public static void main(String[] args){
         System.out.println("Ausgabe aus der main()-Methode");
+        int x = 3;
+        int y = 4;
+        int z = x + y;
     }
 }
